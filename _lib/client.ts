0e99186b64b4addd4a70f748d876830a756cdfc2
Tuomas <tuomas.orhanen@amban.fi>
2023-09-17 22:41:20 +0300

    cdn on

--- _lib/client.ts
+++ _lib/client.ts
@@ -1,15 +1,8 @@
 import { createClient } from 'next-sanity';
-import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
   apiVersion: '2022-11-28',
   dataset: 'production',
   projectId: '0ciqok3x',
-  useCdn: false,
+  useCdn: true,
 });
-
-const builder = imageUrlBuilder(client);
-
-export function urlFor(source: any) {
-  return builder.image(source);
-}
